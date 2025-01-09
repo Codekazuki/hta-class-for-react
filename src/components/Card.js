36c0f3f24e952c72cbeae1c0900132e5ddd6893b
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-01-09 17:21:30 +0100

    hta react class

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
-import obf from "../asset/images/obj.jfif";
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div>
-      <h2>Olusegun Obasanjo</h2>
-      <img src={obf} alt='' />
-      <h4>1999-2003</h4>
+    <div className='card'>
+      <h2>{props.job}</h2>
+      <img src={props.img} alt={props.name} />
+      <h4>{props.name}</h4>
+      <h1>{props.age}</h1>
       <h4>PDP</h4>
       <h5>Atiku Abubakar</h5>
     </div>
