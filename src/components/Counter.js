1b9c05d4d979a891d250d28449475756d72905d6
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-01-27 19:44:44 +0100

    hta class

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,36 @@
 import React, { useState } from "react";
 import data from "../data";
+console.log(data);
 
 const Counter = () => {
+  const [people, setPeople] = useState(data);
+  const clearList = () => {
+    setPeople([]);
+  };
+  const reloadList = () => {
+    setPeople(data);
+  };
+  const deletePerson = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
   return (
     <section>
       <p>List of my friends</p>
+
+      <ul>
+        {people.map((person) => {
+          const { id, name } = person;
+          return (
+            <span key={id}>
+              <li>{name}</li>
+              <button onClick={() => deletePerson(id)}>delete</button>
+            </span>
+          );
+        })}
+      </ul>
+      <button onClick={clearList}>clear</button>
+      <button onClick={reloadList}>reload</button>
     </section>
   );
 };
